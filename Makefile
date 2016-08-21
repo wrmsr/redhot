b1348b8dd9a321e1d692373662f380197a36a74d
wrmsr <timwilloney@gmail.com>
2016-08-21 13:25:56 -0700

    --

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 all: clean test flake8 pre-commit
-.PHONY: all
+.PHONY: all dev
 
 clean:
+	rm -rf .dev
 	rm -rf .tox
 	rm -rf *.egg-info
 	find redhot tests -name '*.pyc' -delete -or -name '*.pyo' -delete -or -name '__pycache__' -delete
@@ -15,3 +16,6 @@ flake8:
 pre-commit:
 	tox -e pre-commit
 
+.dev:
+	virtualenv .dev
+	bash -c 'source .dev/bin/activate && pip install -r requirements-dev.txt'
--- redhot/serve.py
+++ redhot/serve.py
@@ -2,24 +2,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import json
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 # https://api.github.com/users/google/repos
 # curl \
-#     -G "https://api.github.com/search/repositories" \
-#     --data-urlencode "sort=stars" \
-#     --data-urlencode "order=desc" \
-#     --data-urlencode "q=language:java" \
-#     --data-urlencode "q=created:>`date -v-7d '+%Y-%m-%d'`"
+#     -G 'https://api.github.com/search/repositories' \
+#     --data-urlencode 'sort=stars' \
+#     --data-urlencode 'order=desc' \
+#     --data-urlencode 'q=language:java' \
+#     --data-urlencode 'q=created:>`date -v-7d '+%Y-%m-%d'`'
 
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def hello():
-    return "Hello World!"
+    return json.dumps({'response': 'Hello World!'})
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
